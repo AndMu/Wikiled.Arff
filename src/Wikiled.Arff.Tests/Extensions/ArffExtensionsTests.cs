83387d69891a3dcdc11439e86b9840f5781c88b3
Andrius <andrius@wikiled.com>
2017-06-12 12:46:00 +0100

    Adding extensions

---
+++ src/Wikiled.Arff.Tests/Extensions/ArffExtensionsTests.cs
@@ -0,0 +1,91 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Wikiled.Arff.Extensions;
+using Wikiled.Arff.Persistence;
+
+namespace Wikiled.Arff.Tests.Extensions
+{
+    [TestFixture]
+    public class ArffExtensionsTests
+    {
+		[Test]
+        public void RemoveClass()
+        {
+            var dataSet = ArffDataSet.CreateSimple("Test");
+            dataSet.Header.RegisterNominalClass("One", "Two", "Three");
+            var review = dataSet.AddDocument();
+            review.Class.Value = "One";
+            review = dataSet.AddDocument();
+            review.Class.Value = "Two";
+            review = dataSet.AddDocument();
+            review.Class.Value = "Three";
+            dataSet.RemoveClass("Two");
+            Assert.AreEqual(2, dataSet.Documents.Count());
+            Assert.AreEqual("@RELATION Test\r\n@ATTRIBUTE class {One, Three}\r\n@DATA", dataSet.ToString());
+        }      
+		
+        [Test]
+        public void CompactHeader()
+        {
+            var dataSet = ArffDataSet.CreateSimple("Test");
+            dataSet.Header.RegisterNumericClass();
+            dataSet.Header.CreateHeader = true;
+            for (int i = 0; i < 10; i++)
+            {
+                var review = dataSet.AddDocument();
+                review.AddRecord("One");
+            }
+
+            var review2 = dataSet.AddDocument();
+            review2.AddRecord("Two");
+            Assert.AreEqual(3, dataSet.Header.Total);
+            Assert.AreEqual(11, dataSet.Documents.Count());
+            dataSet.CompactHeader(5);
+            Assert.AreEqual(2, dataSet.Header.Total);
+            Assert.AreEqual(10, dataSet.Documents.Count());
+            Assert.AreEqual(10, dataSet.Header["One"].InDocuments);
+        }
+
+        [Test]
+        public void CompactReviews()
+        {
+            var dataSet = ArffDataSet.CreateSimple("Test");
+            dataSet.UseTotal = true;
+            dataSet.Header.RegisterNumericClass();
+            dataSet.Header.CreateHeader = true;
+            for (int i = 0; i < 10; i++)
+            {
+                var review = dataSet.AddDocument();
+                review.AddRecord("One");
+            }
+
+            var review2 = dataSet.AddDocument();
+            review2.AddRecord("Two");
+            review2.AddRecord("Three");
+            review2.AddRecord("Three2");
+            Assert.AreEqual(5, dataSet.Header.Total);
+            Assert.AreEqual(11, dataSet.Documents.Count());
+            dataSet.CompactReviews(2);
+            Assert.AreEqual(5, dataSet.Header.Total);
+            Assert.AreEqual(1, dataSet.Documents.Count());
+            Assert.AreEqual("{1 1,2 1,3 1}", dataSet.Documents.First().ToString());
+            Assert.AreEqual(1, dataSet.Header["Two"].InDocuments);
+            Assert.AreEqual(0, dataSet.Header["One"].InDocuments);
+        }
+
+        [Test]
+        public void CompactClass()
+        {
+            var dataSet = ArffDataSet.CreateSimple("Test");
+            dataSet.Header.RegisterNominalClass("One", "Two", "Three");
+            var review = dataSet.AddDocument();
+            review.Class.Value = "One";
+            review = dataSet.AddDocument();
+            review.Class.Value = "Two";
+            review = dataSet.AddDocument();
+            review.Class.Value = "Two";
+            dataSet.CompactClass(1);
+            Assert.AreEqual(2, dataSet.Documents.Count());
+        }
+    }
+}
---
+++ src/Wikiled.Arff/Extensions/ArffExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using Wikiled.Arff.Persistence;
+using Wikiled.Arff.Persistence.Headers;
+using Wikiled.Core.Utility.Arguments;
+
+namespace Wikiled.Arff.Extensions
+{
+    public static class ArffExtensions
+    {
+        public static void CompactClass(this IArffDataSet dataSet, int minimum)
+        {
+            Guard.NotNull(() => dataSet, dataSet);
+            var classItem = dataSet.Header.Class as NominalHeader;
+            if (classItem == null)
+            {
+                throw new InvalidOperationException("Class not supported");
+            }
+
+            foreach (var nominal in classItem.Nominals)
+            {
+                var count = dataSet.Documents.Count(item => item.Class.Header.ReadValue(item.Class) == nominal);
+                if (count <= minimum)
+                {
+                    dataSet.RemoveClass(nominal);
+                }
+            }
+        }
+
+        public static void RemoveClass(this IArffDataSet dataSet, string name)
+        {
+            Guard.NotNull(() => dataSet, dataSet);
+            Guard.NotNullOrEmpty(() => name, name);
+            var classItem = dataSet.Header.Class as NominalHeader;
+            if (classItem == null)
+            {
+                throw new InvalidOperationException("Class not supported");
+            }
+
+            var reviews = dataSet.Documents.ToArray();
+            foreach (var review in reviews)
+            {
+                if (review.Class.Header.ReadValue(review.Class) == name)
+                {
+                    dataSet.RemoveDocument(review.Id);
+                }
+            }
+
+            classItem.Nominals = classItem.Nominals.Where(item => item != name).ToArray();
+        }
+
+        public static void CompactHeader(this IArffDataSet dataSet, int minOccurences)
+        {
+            Guard.NotNull(() => dataSet, dataSet);
+            foreach (var header in dataSet.Header.ToArray())
+            {
+                if (header == dataSet.Header.Class)
+                {
+                    continue;
+                }
+
+                if (header.InDocuments <= minOccurences)
+                {
+                    dataSet.Header.Remove(header);
+                }
+            }
+
+            dataSet.CompactReviews(0);
+        }
+
+        public static void CompactReviews(this IArffDataSet dataSet, int minReviewSize)
+        {
+            Guard.NotNull(() => dataSet, dataSet);
+            foreach (var review in dataSet.Documents.ToArray())
+            {
+                if (review.Count <= minReviewSize)
+                {
+                    dataSet.RemoveDocument(review.Id);
+                }
+            }
+        }
+    }
+}
