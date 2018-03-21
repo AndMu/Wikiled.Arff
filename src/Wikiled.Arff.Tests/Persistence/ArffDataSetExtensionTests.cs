ab1979f24cc0516aae4a824903d38963af260b9b
Andrius Mudinas <keistokas@gmail.com>
2018-03-21 19:40:30 +0000

    Update vector generation

--- src/Wikiled.Arff.Tests/Persistence/ArffDataSetExtensionTests.cs
+++ src/Wikiled.Arff.Tests/Persistence/ArffDataSetExtensionTests.cs
@@ -14,6 +14,8 @@ namespace Wikiled.Arff.Tests.Persistence
             var dataSet = ArffDataSet.LoadSimple(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", @"problem.arff"));
             var data = dataSet.GetData().ToArray();
             Assert.AreEqual(9371, data.Length);
+            Assert.AreEqual(8, data[0].X.Length);
+            Assert.AreEqual(0, data[0].Y);
         }
 
         [Test]
@@ -22,6 +24,37 @@ namespace Wikiled.Arff.Tests.Persistence
             var dataSet = ArffDataSet.LoadSimple(Path.Combine(TestContext.CurrentContext.TestDirectory, "Data", @"Data.arff"));
             var data = dataSet.GetData().ToArray();
             Assert.AreEqual(7215, data.Length);
+            Assert.AreEqual(456, data[0].X.Length);
+            Assert.AreEqual(2, data[0].Y);
+        }
+
+        [Test]
+        public void GetDataNoClass()
+        {
+            var dataset = ArffDataSet.CreateSimple("Test");
+            var doc = dataset.AddDocument();
+            doc.AddRecord("Test1");
+            doc.AddRecord("Test2");
+            var data = dataset.GetData().ToArray();
+            Assert.AreEqual(1, data.Length);
+            Assert.AreEqual(2, data[0].X.Length);
+            Assert.IsNull(data[0].Y);
+        }
+
+        [Test]
+        public void GetSimpleData()
+        {
+            var dataset = ArffDataSet.CreateSimple("Test");
+            dataset.Header.RegisterDate("Date");
+            dataset.Header.RegisterNumericClass();
+            var doc = dataset.AddDocument();
+            doc.AddRecord("Test1");
+            doc.AddRecord("Test2");
+            doc.Class.Value = 1;
+            var data = dataset.GetData().ToArray();
+            Assert.AreEqual(1, data.Length);
+            Assert.AreEqual(2, data[0].X.Length);
+            Assert.AreEqual(1, data[0].Y);
         }
     }
 }
--- src/Wikiled.Arff/Persistence/ArffDataSetExtension.cs
+++ src/Wikiled.Arff/Persistence/ArffDataSetExtension.cs
@@ -7,26 +7,39 @@ namespace Wikiled.Arff.Persistence
 {
     public static class ArffDataSetExtension
     {
-        public static IEnumerable<(int Y, double[] X)> GetData(this IArffDataSet dataSet)
+        public static IEnumerable<(int? Y, double[] X)> GetData(this IArffDataSet dataSet)
         {
+            var headers = dataSet.Header.Where(item => dataSet.Header.Class != item && !(item is DateHeader));
+            var table = new Dictionary<IHeader, int>();
+            int index = 0;
+            foreach (var header in headers)
+            {
+                table[header] = index;
+                index++;
+            }
+
             foreach (var dataRow in dataSet.Documents)
             {
-                int y = dataSet.Header.Class.ReadClassIdValue(dataRow.Class);
-                yield return (y, dataRow.GetX());
+                var y = dataSet.Header.Class?.ReadClassIdValue(dataRow.Class);
+                yield return (y, dataRow.GetX(table));
             }
         }
 
-        public static double[] GetX(this IArffDataRow row)
+        private static double[] GetX(this IArffDataRow row, Dictionary<IHeader, int> headers)
         {
-            double[] x = new double[row.Owner.Header.Total];
+            double[] x = new double[headers.Count];
             for (int i = 0; i < x.Length; i++)
             {
                 x[i] = 0;
             }
 
-            foreach (var wordsData in row.GetRecords().Where(item => !(item.Header is DateHeader)))
+            foreach (var wordsData in row.GetRecords())
             {
-                int index = row.Owner.Header.GetIndex(wordsData.Header);
+                if (!headers.TryGetValue(wordsData.Header, out var index))
+                {
+                    continue;
+                }
+
                 double value = 1;
                 if (wordsData.Value != null)
                 {
